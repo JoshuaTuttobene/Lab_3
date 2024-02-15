128238ab60b49d975e1f7c290fc8f893f1503453
aaron-esc-13 <156145840+aaron-esc-13@users.noreply.github.com>
2024-02-15 15:26:45 -0800

    Create CL_Proportional_Control.py

---
+++ src/CL_Proportional_Control.py
@@ -0,0 +1,12 @@
+import encoder_reader as ER
+import pyb
+import micropython
+import motor_driver as MD
+
+class ClosedLoop
+
+    def __init__ (self, Kp,setpoint):
+    
+    def run(self)
+    
+    def set_setpoint
